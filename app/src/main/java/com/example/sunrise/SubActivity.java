6ea7cd95bbe402440a6116ac64029200ea8051ec
Lee Chae Lin <48716540+Chaenini@users.noreply.github.com>
2020-04-23 00:33:06 +0900

    로고 클릭 이벤트
    하 .. 드디어 성공했다 ..ㅎ

--- app/src/main/java/com/example/sunrise/SubActivity.java
+++ app/src/main/java/com/example/sunrise/SubActivity.java
@@ -9,6 +9,6 @@ public class SubActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sub);
+        setContentView(R.layout.activity_sub);
     }
 }
